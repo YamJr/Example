94010bbfb95118041847ad24ff3c9df8825af305
YamJr <yamgrg2058@gmail.com>
2023-09-01 20:18:37 +0545

    ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './Component/Navbar/Navbar'
+import Home from './Component/Home/Home'
 
 function App() {
   return (
-    <>
+    <div div className='app'>
       <Navbar />
-    </>
+      <Home />
+    </div>
   )
 }
 
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -8,6 +8,14 @@ function Navbar(){
             <div className="navbar-brand">
           <img src={logo} alt="nike" className="nike-logo" />
         </div>
+        <div className="nav-menu">
+           <li className="navbar-item"><a href="#">Men</a></li>
+           <li className="navbar-item"><a href="#">Women</a></li>
+           <li className="navbar-item"><a href="#">Kids</a></li>
+        </div>
+        <div className="user-info">
+            <p>hello</p>
+        </div>
         </div>
     )
 }
